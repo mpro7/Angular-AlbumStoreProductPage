f84f50e06ce98021e4e8329f47cff862dc3e0ee0
mpro7 <justinstarlight@gmail.com>
2019-03-12 22:49:00 +0100

    formatting changes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Album } from "./album";
+import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
-import { Product } from "./product";
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
